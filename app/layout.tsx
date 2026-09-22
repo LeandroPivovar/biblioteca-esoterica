575877ecd681835efc91db0b7ad5427654ee7ee5
LeandroPivovar <leandropivovar@hotmail.com>
2026-09-22 17:39:56 -0300

    feat(analytics): add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,5 +22,40 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="pt-BR"><body>{children}</body></html>;
+  return (
+    <html lang="pt-BR">
+      <head>
+        {/* Meta Pixel - PageView; checkout clicks are tracked in app/page.tsx. */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1452336476714044');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
+      <body>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1452336476714044&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
+    </html>
+  );
 }
